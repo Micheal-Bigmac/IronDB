a04304e566e6103fccb18ffa1a99a7056226c255
micheal-bigmac <2452064683@qq.com>
2017-10-12 21:24:22 +0800

    Client Dynamic proxy

--- metastore/src/test/java/com/irondb/metastore/rpc/http/testHttp.java
+++ metastore/src/test/java/com/irondb/metastore/rpc/http/testHttp.java
@@ -3,7 +3,6 @@ package com.irondb.metastore.rpc.http;
 import com.alibaba.fastjson.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 
